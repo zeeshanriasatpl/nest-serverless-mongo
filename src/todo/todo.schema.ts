058d6712c3f3a97d99c760e3137a5dc103196a6c
Zeeshan Riasat <link.zeeshan.riasat@gmail.com>
2025-04-24 11:19:55 +0500

    todo schema

---
+++ src/todo/todo.schema.ts
@@ -0,0 +1,21 @@
+import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
+import { Document } from 'mongoose';
+
+export type TodoDocument = Todo & Document;
+
+@Schema()
+export class Todo {
+  @Prop({ required: true })
+  title: string;
+
+  @Prop()
+  description?: string;
+
+  @Prop({ default: false })
+  completed: boolean;
+
+  @Prop({ default: Date.now })
+  createdAt: Date;
+}
+
+export const TodoSchema = SchemaFactory.createForClass(Todo);
